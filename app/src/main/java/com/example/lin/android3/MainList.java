7f07c4d0f5a8f82401f0d528caed7246f7319d0f
blin <bennylin@outlook.com>
2015-01-29 17:19:56 +0800

    t

--- app/src/main/java/com/example/lin/android3/MainList.java
+++ app/src/main/java/com/example/lin/android3/MainList.java
@@ -1,9 +1,17 @@
 package com.example.lin.android3;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainList extends ActionBarActivity {
@@ -35,5 +43,69 @@ public class MainList extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+ class NewsItem {
+    private String Title;
+    private String Address;
+    public String getTitle(){
+        return Title;
+    }
+    public String getAddress(){
+        return Address;
+    }
+
+    public void setTitle(String title  )
+    {
+        this.Title=title;
+    }
+    public void setAddress(String address)
+    {
+        this.Address=address;
+    }
+    @Override
+    public String toString(){
+        return Title+":"+Address;
+    }
+}
+class CustomListAdapter extends BaseAdapter {
+private ArrayList<NewsItem> listData;
+private LayoutInflater layoutInflater;
+    public CustomListAdapter(Context context, ArrayList<NewsItem> listData) {
+        this.listData = listData;
+        layoutInflater = LayoutInflater.from(context);
+
+    }
+    @Override
+    public int getCount() {
+        return listData.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return listData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.list_row_layout, null);
+            holder = new ViewHolder();
+            holder.headlineView = (TextView) convertView.findViewById(R.id.title);
+            holder.reporterNameView = (TextView) convertView.findViewById(R.id.reporter);
+            holder.reportedDateView = (TextView) convertView.findViewById(R.id.date);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.headlineView.setText(listData.get(position).getHeadline());
+        holder.reporterNameView.setText("By, " + listData.get(position).getReporterName());
+        holder.reportedDateView.setText(listData.get(position).getDate());
+
+        return convertView;
     }
 }
