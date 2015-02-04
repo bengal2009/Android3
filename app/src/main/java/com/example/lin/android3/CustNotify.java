95984586dedb2b2cc3d5a9666a2dc50f4be4836a
blin <bennylin@outlook.com>
2015-02-04 15:16:50 +0800

    t

---
+++ app/src/main/java/com/example/lin/android3/CustNotify.java
@@ -0,0 +1,152 @@
+package com.example.lin.android3;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
+
+import android.view.View.OnClickListener;
+
+
+public class CustNotify extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cust_notify);
+        Button bnotify = (Button) findViewById(R.id.notification);
+
+        Button bcustomnotify = (Button) findViewById(R.id.customnotification);
+
+        bnotify.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                Notification();
+                // TODO Auto-generated method stub
+            }
+        });
+
+        bcustomnotify.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                CustomNotification();
+                // TODO Auto-generated method stub
+            }
+        });
+
+    }
+
+    public void Notification() {
+        // Set Notification Title
+        String strtitle = getString(R.string.notificationtitle);
+        // Set Notification Text
+        String strtext = getString(R.string.notificationtext);
+
+        // Open NotificationView Class on Notification Click
+        Intent intent = new Intent(this, NotificationView.class);
+        // Send data to NotificationView Class
+        intent.putExtra("title", strtitle);
+        intent.putExtra("text", strtext);
+        // Open NotificationView.java Activity
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Create Notification using NotificationCompat.Builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                // Set Icon
+                .setSmallIcon(R.drawable.ic_launcher)
+                        // Set Ticker Message
+                .setTicker(getString(R.string.notificationticker))
+                        // Set Title
+                .setContentTitle(getString(R.string.notificationtitle))
+                        // Set Text
+                .setContentText(getString(R.string.notificationtext))
+                        // Add an Action Button below Notification
+                .addAction(R.drawable.ic_launcher, "Action Button", pIntent)
+                        // Set PendingIntent into Notification
+                .setContentIntent(pIntent)
+                        // Dismiss Notification
+                .setAutoCancel(true);
+
+        // Create Notification Manager
+        NotificationManager notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Build Notification with Notification Manager
+        notificationmanager.notify(0, builder.build());
+
+    }
+
+    public void CustomNotification() {
+        // Using RemoteViews to bind custom layouts into Notification
+        RemoteViews remoteViews = new RemoteViews(getPackageName(),
+                R.layout.customnotification);
+
+        // Set Notification Title
+        String strtitle = getString(R.string.customnotificationtitle);
+        // Set Notification Text
+        String strtext = getString(R.string.customnotificationtext);
+
+        // Open NotificationView Class on Notification Click
+        Intent intent = new Intent(this, NotificationView.class);
+        // Send data to NotificationView Class
+        intent.putExtra("title", strtitle);
+        intent.putExtra("text", strtext);
+        // Open NotificationView.java Activity
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                // Set Icon
+                .setSmallIcon(R.drawable.ic_launcher)
+                        // Set Ticker Message
+                .setTicker("1111")
+                        // Dismiss Notification
+                .setAutoCancel(true)
+                        // Set PendingIntent into Notification
+                .setContentIntent(pIntent)
+                        // Set RemoteViews into Notification
+                .setContent(remoteViews);
+
+        // Locate and set the Image into customnotificationtext.xml ImageViews
+        remoteViews.setImageViewResource(R.id.imagenotileft,R.drawable.ic_launcher);
+        remoteViews.setImageViewResource(R.id.imagenotiright,R.drawable.ic_launcher);
+
+        // Locate and set the Text into customnotificationtext.xml TextViews
+        remoteViews.setTextViewText(R.id.title,"22222");
+        remoteViews.setTextViewText(R.id.text,"1111");
+
+        // Create Notification Manager
+        NotificationManager notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Build Notification with Notification Manager
+        notificationmanager.notify(0, builder.build());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cust_notify, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
